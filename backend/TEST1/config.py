3556ec403ed48ad6b630fcbc9a763d0f86cd7d35
gabriellemuller <g.muller@alumnos.upm.es>
2025-11-02 23:00:33 +0100

    update

---
+++ backend/TEST1/config.py
@@ -0,0 +1,9 @@
+# Fichier config.py
+# --- Paramètres de connexion à la Base de Données ---
+# IMPORTANT: Remplacez ces valeurs par vos identifiants réels.
+
+DB_HOST = "138.100.82.184"
+DB_PORT = "5432"
+DB_NAME = "2207"
+DB_USER = "lectura"
+DB_PASSWORD = "ncorrea#2022"
