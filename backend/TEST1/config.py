b51b98124eb703ea5d66934d185b3e8938ed1d76
gabriellemuller <g.muller@alumnos.upm.es>
2025-11-03 15:56:35 +0100

    gitignore test

--- backend/TEST1/config.py
+++
@@ -1,9 +0,0 @@
-# Fichier config.py
-# --- Paramètres de connexion à la Base de Données ---
-# IMPORTANT: Remplacez ces valeurs par vos identifiants réels.
-
-DB_HOST = "138.100.82.184"
-DB_PORT = "2345"
-DB_NAME = "2207"
-DB_USER = "lectura"
-DB_PASSWORD = "X"
